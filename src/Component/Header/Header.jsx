ae998c6d977fa086ad24a3151f92f44e8d8acaa6
amranhossain <amranhossainchy@gmail.com>
2022-12-29 11:21:07 +0600

    sizing issue solved

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-scroll';
 import headerImage from './../Image/PicturefINAL.png';
 import navbarAvatar from './../Image/IMG_20220416_205320 (2).jpg';
 import Typewriter from 'typewriter-effect';
+import { FiDownload } from "react-icons/fi";
 
 
 
@@ -11,14 +12,14 @@ const Header = () => {
         <div id='home' className='lg:h-screen'>
             <div className="navbar lg:p-2 p-0">
                 <div className="navbar-start">
-                    <div className="dropdown dropdown-end">
+                    <div className="dropdown dropdown-end lg:block hidden">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img src={navbarAvatar} />
                             </div>
                         </label>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl text-white uppercase lg:p-1 p-0">Portfo<span className='text-rose-500'>lio</span></a>
+                    <a className="btn btn-ghost normal-case text-xl text-md text-white uppercase lg:p-1 p-0">Portfo<span className='text-rose-500'>lio</span></a>
                 </div>
 
                 <div className="navbar-center hidden lg:flex">
@@ -39,7 +40,7 @@ const Header = () => {
                 </div>
                 <div className="navbar-end ">
 
-                    <a className="btn btn-sm rounded-full text-white bg-rose-600 hover:bg-zinc-900 hover:text-rose-500 hover:outline">Download CV</a>
+                    <a className="btn btn-sm rounded-full text-white bg-rose-600 normal-case hover:bg-zinc-900 hover:text-rose-500 hover:outline">Resume <FiDownload /> </a>
                 </div>
             </div>
             <div className='flex justify-between place-items-center mt-10 '>
